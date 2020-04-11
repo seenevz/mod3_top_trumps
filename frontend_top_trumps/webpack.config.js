8d0fd5bdce5fbf3f2659202eff895879663408fd
Sergio Neves <sernevess@gmail.com>
2020-04-12 00:05:28 +0100

    added webpack configurations for development

--- frontend_top_trumps/webpack.config.js
+++ frontend_top_trumps/webpack.config.js
@@ -1,13 +1,22 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build"),
   },
-  plugins: [new HtmlWebpackPlugin({ title: "Top Trumps" })],
+  devtool: "eval-source-map",
+  devServer: {
+    contentBase: "./build",
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ title: "Top Trumps", template: "src/index.html" }),
+  ],
   module: {
     rules: [
       {
